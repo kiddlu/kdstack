b0485431cf0b34cacb484730ecdbc13eb37b90f3
kiddlu <kidd.dawny.lu@gmail.com>
2018-06-11 21:03:38 +0800

    update
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

---
+++ python/mix-program/call-so/simple.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+#use ctype, which is like jni
+
+cat <<EOF > pycall.c
+/* gcc -o libpycall.so -shared -fPIC pycall.c */
+#include <stdio.h>
+#include <stdlib.h>
+
+struct cstruct
+{
+    int i;
+    float d;
+    struct cstruct *self;
+};
+
+int add(int a, int b)
+{
+    //printf("you input %d and %d\n", a, b);
+    return a+b;
+}
+
+void swap(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+    return;
+}
+
+struct cstruct *init(void)
+{
+    struct cstruct *p = malloc(sizeof(struct cstruct));
+    p->i = 6;
+    p->d = 3.0;
+    p->self = p;
+    return p;
+}
+
+void change(struct cstruct *p)
+{
+    p->i = 9;
+    p->d = 29.0;
+    p->self = NULL;
+    return;
+}
+
+void final(struct cstruct *p)
+{
+    free(p);
+    return;
+}
+EOF
+
+gcc -o libpycall.so -shared -fPIC pycall.c
+
+cat <<EOF > pycall.py
+from ctypes import *
+
+class Cstruct(Structure):
+    pass
+
+ll = cdll.LoadLibrary
+lib = ll("./libpycall.so")
+a = c_int(3)
+b = c_int(7)
+
+ret = lib.add(a, b)
+print(ret)
+
+lib.swap(byref(a), byref(b))
+print(a, b)
+print(a.value, b.value)
+
+Cstruct._fields_ = [("i", c_int),
+                    ("d", c_float),
+                    ("self", POINTER(Cstruct))]
+lib.init.restype = POINTER(Cstruct)
+
+cs = lib.init()
+print(cs.contents.i, cs.contents.d, cs.contents.self)
+cs_self = cs.contents.self
+print(cs_self.contents.i, cs_self.contents.d, cs_self.contents.self)
+
+lib.change(cs)
+print(cs.contents.i, cs.contents.d, cs.contents.self)
+
+if cs.contents.self == (POINTER(c_int)()):
+    print("self is NULL")
+else:
+    print("self is NOT null")
+
+
+lib.final(cs)
+EOF
+
+python3 pycall.py
+rm pycall.py pycall.c libpycall.so
